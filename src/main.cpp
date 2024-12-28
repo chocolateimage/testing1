dd6a45c70d6d48e5d40d1444ecef02ce964cd40f
chocolateimage <chocolateimage@protonmail.com>
2024-12-28 13:05:35 +0100

    Add a bit more code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
 
 int main() {
+    std::srand(std::time(nullptr));
     std::cout << "Hello world!" << std::endl;
+    std::vector<std::string> example;
+    int max = (std::rand() / (RAND_MAX / 1000));
+    for (int i = 0; i < max; i++) {
+        example.push_back(std::to_string(std::rand()));
+    }
+    for (auto x : example) {
+        std::cout << x << std::endl;
+    }
+    
     return 0;
 }
