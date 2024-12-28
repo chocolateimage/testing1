637a3c8317ca9efa996b2a9ea24a7db6e09bbfb2
chocolateimage <chocolateimage@protonmail.com>
2024-12-28 17:36:02 +0100

    Add argument for max amount of numbers to print

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 #include <iostream> // For printing to stdout
+#include <stdexcept>
+#include <string>
 #include <vector> // For lists
 #include <ctime> // Needed for setting random value to current time
 
-int main() { // Entry point
+void show_usage(char** argv) {
+    std::cerr << "Usage: " << argv[0] << " <max amount>" << std::endl;
+}
+
+int main(int argc, char** argv) { // Entry point
+    if (argc != 2) {
+        show_usage(argv);
+        return 1;
+    }
+
+    int maxAmount;
+    try {
+        maxAmount = std::stoi(argv[1]);
+    } catch (std::invalid_argument&) {
+        show_usage(argv);
+        std::cerr << "Error: max amount must be a number" << std::endl;
+        return 1;
+    }
+
     std::srand(std::time(nullptr)); // Setting the random value to current time
     std::cout << "Hello, World!" << std::endl; // The text "Hello world!" gets written to stdout with a newline
     std::vector<std::string> example; // Create a list for later
-    int max = (std::rand() / (RAND_MAX / 15)); // Set a max variable for the for loop, set it here because in the for loop it gets executed every iteration
-    for (int i = 0; i < max; i++) { // The for loop
+    int amount = (std::rand() / (RAND_MAX / maxAmount)); // Set a max variable for the for loop, set it here because in the for loop it gets executed every iteration
+    for (int i = 0; i < amount; i++) { // The for loop
         example.push_back(std::to_string(std::rand())); // Add a random value as a string to the list
     }
     for (auto x : example) { // Iterate over every random number
