908ad7eb9d20ce97e3dba360b4d9ed247b12cdc6
chocolateimage <chocolateimage@protonmail.com>
2024-12-31 20:13:02 +0100

    add debug prints

--- tools/gittest.sh
+++ tools/gittest.sh
@@ -1,6 +1,13 @@
+echo "Fetching..."
 git fetch
+echo "Switching branch..."
 git checkout main
+echo "Appending..."
 echo Test >> test.txt
+echo "Adding..."
 git add test.txt
+echo "Committing..."
 git commit -m "Append to test.txt"
+echo "Pushing..."
 git push
+echo "Done!"
