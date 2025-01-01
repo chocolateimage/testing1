ed8821dc36a8d4603be2e5b7998ca38dfcdf65b2
chocolateimage <chocolateimage@protonmail.com>
2025-01-01 15:25:13 +0100

    Add explanation in check.py

--- tools/check.py
+++ tools/check.py
@@ -6,6 +6,7 @@ has_error = False
 for i in Path(".").rglob("*.cpp"):
     with i.open() as f:
         for lineindex,v in enumerate(f.read().splitlines()):
+            # Every line that has at least 5 characters must have a comment
             if len(v.strip()) > 5:
                 if "//" not in v and "/*" not in v and "*/" not in v:
                     print(str(i) + ":" + str(lineindex + 1) + " - Missing comment")
